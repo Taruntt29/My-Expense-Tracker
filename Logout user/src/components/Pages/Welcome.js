b7fffd3866af3409b3f1af3a826719863dba60c9
Taruntt29 <109497779+Taruntt29@users.noreply.github.com>
2022-09-04 16:59:41 +0530

    Add files via upload

---
+++ Logout user/src/components/Pages/Welcome.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import './Welcome.css'
+const Welcome = (props) => {
+    const history  = useHistory();
+    function logoutHandler(){
+        localStorage.clear();
+        history.replace('/Login')
+
+    };
+    return(
+        <>
+    <div className="header">
+    <div className="welcomeContent" >
+        Welcome to Expense Tracker!!!
+    </div>
+    <div className="profileButton">Your Profile is Incomplete <Link to='/profile'>Complete Now?</Link></div>
+    </div>
+    <button onClick={logoutHandler}>Log Out</button>
+    </>
+    )
+}
+ 
+export default Welcome;
