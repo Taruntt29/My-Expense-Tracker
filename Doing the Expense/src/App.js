b97d15d6ad6b6aeaabc882743fe6b6f35cad51d1
Taruntt29 <109497779+Taruntt29@users.noreply.github.com>
2022-09-04 16:56:31 +0530

    Add files via upload

---
+++ Doing the Expense/src/App.js
@@ -0,0 +1,32 @@
+import { Redirect, Route,Switch } from "react-router-dom";
+import AuthForm from "./components/Auth/AuthForm";
+import ForgotPassword from "./components/Pages/ForgotPassword";
+import Profile from "./components/Pages/Profile";
+import Welcome from "./components/Pages/Welcome";
+function App() {
+  return (
+    <div>
+
+      <Switch>
+      <Route path="/Welcome" exact>
+        <Welcome/>
+      </Route>
+      <Route path="/Login">
+      <AuthForm/>
+      </Route>
+      <Route path="/" exact>
+        <Redirect to="/Login"/>
+      </Route>
+      <Route path='/profile'>
+      
+        <Profile></Profile>
+        </Route>
+      <Route path='/forgotpassword'>
+        <ForgotPassword/>
+      </Route>
+     
+      </Switch>
+    </div>
+  );
+}
+export default App;
---
+++ Doing the Expense/src/components/Pages/Welcome.js
@@ -0,0 +1,28 @@
+import { Link, useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from "../../store";
+import './Welcome.css'
+import Expenses from "./Expenses";
+const Welcome = (props) => {
+    dispatch = useDispatch();
+    const history  = useHistory();
+    function logoutHandler(){
+        localStorage.clear();
+        dispatch(authActions.logout())
+
+        history.replace('/Login')
+
+    };
+    return(
+        <>
+    <div className="header">
+    
+    <Link className="profileButton" to='/profile'>PROFILE</Link>
+    </div>
+    <button className="logoutbtn" onClick={logoutHandler}>Log Out</button>
+    <Expenses></Expenses>
+    </>
+    )
+}
+ 
+export default Welcome;
